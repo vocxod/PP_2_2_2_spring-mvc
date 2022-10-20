4cc3ca70f83aaa463fe1f7e9f7a1a7a5bfad944c
Sergey Ryabinin <ryabinin.sergey@gmail.com>
2022-10-20 15:07:47 +0300

    add some log info

--- src/main/java/web/config/AppInit.java
+++ src/main/java/web/config/AppInit.java
@@ -4,26 +4,28 @@ import org.springframework.web.servlet.support.AbstractAnnotationConfigDispatche
 
 public class AppInit extends AbstractAnnotationConfigDispatcherServletInitializer {
 
-    // Метод, указывающий на класс конфигурации
-    @Override
-    protected Class<?>[] getRootConfigClasses() {
-        return null;
-    }
+  // Метод, указывающий на класс конфигурации
+  @Override
+  protected Class<?>[] getRootConfigClasses() {
+    logger.info("\u001B[33m root config start. \u001B[0m");
+    return null;
+  }
 
+  // Добавление конфигурации, в которой инициализируем ViewResolver, для
+  // корректного отображения jsp.
+  @Override
+  protected Class<?>[] getServletConfigClasses() {
+    logger.info("\u001B[33m servlet config start. \u001B[0m");
+    return new Class<?>[] {
+        WebConfig.class
+    };
+  }
 
-    // Добавление конфигурации, в которой инициализируем ViewResolver, для корректного отображения jsp.
-    @Override
-    protected Class<?>[] getServletConfigClasses() {
-        return new Class<?>[]{
-                WebConfig.class
-        };
-    }
-
-
-    /* Данный метод указывает url, на котором будет базироваться приложение */
-    @Override
-    protected String[] getServletMappings() {
-        return new String[]{"/"};
-    }
+  /* Данный метод указывает url, на котором будет базироваться приложение */
+  @Override
+  protected String[] getServletMappings() {
+    logger.info("\u001B[33m Set servlet path start. \u001B[0m");
+    return new String[] { "/" };
+  }
 
 }
