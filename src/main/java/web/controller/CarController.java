1c894f1b237b165272f6b697e5476b62d7d2764a
Sergey Ryabinin <ryabinin.sergey@gmail.com>
2022-10-20 22:00:36 +0300

    autowired carservice into catcontroller

--- src/main/java/web/controller/CarController.java
+++ src/main/java/web/controller/CarController.java
@@ -2,6 +2,7 @@ package web.controller;
 
 import org.apache.logging.log4j.Logger;
 import org.apache.logging.log4j.LogManager;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -11,10 +12,16 @@ import java.util.ArrayList;
 import java.util.List;
 import java.time.LocalTime;
 
+import web.model.Car;
+import web.service.CarService;
+
 @Controller
 public class CarController {
 
   private static final Logger logger = LogManager.getLogger(CarController.class);
+
+  @Autowired
+  private CarService carService;
 
   @GetMapping(value = "/car")
   public String printWelcome(ModelMap model) {
